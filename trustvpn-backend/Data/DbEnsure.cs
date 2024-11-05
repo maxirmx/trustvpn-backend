985b4ef59e649a665a804e9126b65d8db70472d1
Maxim Samsonov <m.samsonov@computer.org>
2024-11-05 14:34:29 +0300

    feat: healthcheck

--- trustvpn-backend/Data/DbEnsure.cs
+++ trustvpn-backend/Data/DbEnsure.cs
@@ -150,8 +150,8 @@ public class DbEnsure
         {
             _logger.LogInformation("Initializing database at 0.1.4");
             Ensure_0_1_4(connection);
-            _logger.LogInformation("Database version 0.2.1");
-            PuVersionUpdate("0.2.1", connection);
+            _logger.LogInformation("Database version 0.2.5");
+            PuVersionUpdate("0.2.5", connection);
         }
         catch (Exception ex)
         {
